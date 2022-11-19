8e54a3735840a6f241d50cdef5f22ceaea953f84
Azu0925 <yamaneko564783@gmail.com>
2022-11-19 18:00:49 +0900

    wip

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,24 @@
-import { Box, Spinner, Center, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Box } from '@chakra-ui/react'
+import { DEFAULT_LAT, DEFAULT_LNG, DEFAULT_ZOOM, MAP_API_KEY } from '../consts/env'
+
+const center = {
+  lat: DEFAULT_LAT,
+  lng: DEFAULT_LNG,
+}
+const zoom = DEFAULT_ZOOM
 
 export const Map = () => {
   return (
-    <Box bgColor="white" h="500px">
-      <Center h="100%" w="100%">
-        <VStack>
-          <Spinner size="lg" />
-          <Text>map loading...</Text>
-        </VStack>
-      </Center>
+    <Box bgColor="white" h="80vh" w="30vw">
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: MAP_API_KEY }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        ></GoogleMapReact>
+      </div>
     </Box>
   )
 }
---
+++ src/consts/env.ts
@@ -0,0 +1,4 @@
+export const MAP_API_KEY = process.env.REACT_APP_MAP_API_KEY ?? ''
+export const DEFAULT_LAT = 34.70003741529549
+export const DEFAULT_LNG = 135.4930570339683
+export const DEFAULT_ZOOM = 11
