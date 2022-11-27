d4c06b611b309faac07e8f867ee80ac4badeff1b
Azu0925 <yamaneko564783@gmail.com>
2022-11-28 02:57:12 +0900

    style

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,29 +1,6 @@
-import {
-  Box,
-  Center,
-  Skeleton,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-  Text,
-  Flex,
-  Spacer,
-} from '@chakra-ui/react'
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { List } from '../components/events/list'
-import { Header } from '../components/headers/Header'
-import { SideMenu } from '../components/sideMenus/SideMenu'
 import { Template } from '../components/Template'
-
-/**
- * HACK:
- * ダッシュボードの枠（Header, SideMenu）を共通コンポーネント化
- */
 
 export const Events = () => {
   return (
