914f451339da3a023092caf50f9b565563c0c56d
Azu0925 <yamaneko564783@gmail.com>
2022-11-29 01:14:14 +0900

    refactor

--- src/pages/events/New.tsx
+++ src/pages/events/New.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
-import { Template } from '../components/Template'
+import { Template } from '../../components/Template'
 
-export const NewEvent = () => {
+export const New = () => {
   return (
     <div>
       <Template>
