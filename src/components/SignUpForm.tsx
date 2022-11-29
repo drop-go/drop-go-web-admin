7ecddb89f4339974dbfe4d0019d95fd15c1186f9
Azu0925 <yamaneko564783@gmail.com>
2022-11-29 12:37:13 +0900

    feature: ユーザ新規作成

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { FormErrorMessage, FormLabel, FormControl, Input, Button, Center, Textarea } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
+import { UserPostRequest } from '../api/interface'
+import axios from 'axios'
+import { API_URL } from '../consts/env'
+
+const url = `${API_URL}/user`
 
 export const SignUpForm = () => {
   const [cookies, setCookie] = useCookies(['token'])
+  const [error, setError] = useState('')
   const {
     register,
     handleSubmit,
@@ -14,25 +20,24 @@ export const SignUpForm = () => {
   const navigate = useNavigate()
   const onSubmit = (data: any) => {
     const { email, password, organization, organizationDescription } = data
-    const body = {
+    const body: UserPostRequest = {
       email: email,
       password: password,
       name: organization,
       description: organizationDescription,
-      iconDataURL: '',
+      iconDataURI: '',
     }
-    // TODO: signup
-    /*
+
     axios
       .post(url, body)
       .then((res) => {
         const token = res.data.token
         setCookie('token', token)
         navigate('/dashboard')
-      }).catch()
-     */
-    setCookie('token', 'dev_token')
-    navigate('/dashboard')
+      })
+      .catch((err) => {
+        setError(`エラー: ${err}`)
+      })
   }
 
   return (
@@ -46,6 +51,7 @@ export const SignUpForm = () => {
           <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="DropGo Admin" width="100%" />
         </h2>
       </Center>
+      <span>{error}</span>
       <FormControl isInvalid={errors.name ? true : false} mb="5px">
         <FormLabel htmlFor="email">メールアドレス</FormLabel>
         <Input
