2813fa64eaf558e943bf719bb1d2037d21ecc659
Azu0925 <yamaneko564783@gmail.com>
2022-11-30 17:42:46 +0900

    wip

--- src/components/events/New.tsx
+++ src/components/events/New.tsx
@@ -1,8 +1,18 @@
-import { Box, Button, Center, Text } from '@chakra-ui/react'
+import { Box, Button, Center, FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export const New = () => {
-  // TODO: 登録処理
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm()
+  const onSubmit = (data: any) => {
+    const { title } = data
+    const body = {}
+    // TODO: 登録処理
+  }
 
   return (
     <Box bgColor="#EDF2F6" h="92vh" w="85vw">
@@ -12,7 +22,24 @@ export const New = () => {
             イベント新規作成
           </Text>
           <form>
-            <Button>作成</Button>
+            <FormControl>
+              <FormLabel>イベントタイトル</FormLabel>
+              <Input
+                id="title"
+                type="text"
+                placeholder="イベント名"
+                {...register('title', { required: '必須項目です' })}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>紹介文</FormLabel>
+            </FormControl>
+            <FormControl>
+              <FormLabel>イベント住所</FormLabel>
+            </FormControl>
+            <Button type="submit" isLoading={isSubmitting}>
+              作成
+            </Button>
           </form>
         </Box>
       </Center>
