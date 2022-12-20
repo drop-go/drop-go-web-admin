6a791e661362a48d68e613397db81ba3d5b172dc
Azu0925 <yamaneko564783@gmail.com>
2022-12-20 19:29:04 +0900

    fix: tokenをグローバルステートで保持

--- src/hooks/useAuthQuery.ts
+++ src/hooks/useAuthQuery.ts
@@ -2,13 +2,16 @@ import axios from 'axios'
 import { useState } from 'react'
 import { useCookies } from 'react-cookie'
 import { useNavigate } from 'react-router-dom'
+import { useSetRecoilState } from 'recoil'
 import { AuthGetRequest } from '../api/interface'
 import { API_URL } from '../consts/env'
+import { authState } from '../globalStates/authState'
 
 const url = `${API_URL}/auth`
 
 export const useAuthQuery = () => {
   const [cookies, setCookie] = useCookies(['token'])
+  const setAuthState = useSetRecoilState(authState)
   const [error, setError] = useState('')
   const navigate = useNavigate()
   const login = (authParams: AuthGetRequest) => {
@@ -17,10 +20,10 @@ export const useAuthQuery = () => {
       .then((res) => {
         const token = res.data.token
         setCookie('token', token)
+        setAuthState(token)
         navigate('/dashboard')
       })
       .catch((err) => {
-        console.log(err)
         setError(`エラー: ${err.response.data.message}`)
       })
   }
